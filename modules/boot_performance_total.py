99f6ef0ab36c4130a4d0e094099945b11eef307e
Joao Gabriel <gois.jg.dev@gmail.com>
2021-02-10 23:15:46 -0300

    Adding colors, refactoring boot_perf and main script

--- modules/boot_performance_total.py
+++ modules/boot_performance_total.py
@@ -2,6 +2,7 @@
 import re
 import os
 from modules.shell_script_launcher import run_shell_script
+from modules.color import colors;
 
 
 def create_raw_blame_file_and_analyze(analyze):
@@ -19,7 +20,7 @@ def create_raw_blame_file_and_analyze(analyze):
     os.remove('/tmp/.blame.txt')
 
 
-def print_perfomance_total(file):
+def apply_regex_in_blame_file(file):
     line_time_digit_clean_list = []
     total = 0
 
@@ -41,7 +42,11 @@ def print_perfomance_total(file):
 
     for task_time_took_in_ms in line_time_digit_clean_list:
         total = total + task_time_took_in_ms
+    
+    return total
 
+
+def parse_total_time_in_formatted_string(total):
     total_in_min = round((total / 60000), 3)
     total_in_sec = total_in_min * 60
 
@@ -50,7 +55,15 @@ def print_perfomance_total(file):
 
     time = total_in_min_string if total_in_min > 1 else total_in_sec_string
 
-    print(f'This time your system took {time} to boot')
+    return time
+
+
+def print_perfomance_total(file):
+    c = colors()
+    total = apply_regex_in_blame_file(file)
+    time = parse_total_time_in_formatted_string(total)
+
+    print(f'{c.BLD}{c.UNDLN}This time your system took {time} to boot{c.END}')
 
 
 def calculate():
